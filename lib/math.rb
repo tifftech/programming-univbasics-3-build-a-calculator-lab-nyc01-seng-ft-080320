651c099f8c1417ee1142eff0a681e15eab941cf6
Tiffani Best <tiffanimelissabest@gmail.com>
2020-07-26 19:42:13 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
+  puts num2 + num1
 
 end
 
 def subtraction(num1, num2)
+  puts num2 - num1
 
 end
 
 def division(num1, num2)
+  puts num2 / num1
 
 end
 
 def multiplication(num1, num2)
+  puts num1 ** num2
 
 end
 
 def modulo(num1, num2)
+  puts num2 % num1
 
 end
 
 def square_root(num)
+  puts math.sqrt(num)
 
 end
