a5b446f574a7517eeb61b395fb9d99385b67d6c4
Tiffani Best <tiffanimelissabest@gmail.com>
2020-07-26 19:57:12 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
 def addition(num1, num2)
+  puts "5 + 4"
+  sum = 9
 
 end
 
 def subtraction(num1, num2)
+  puts "10 - 5"
+  sum = 5
 
 end
 
 def division(num1, num2)
+  puts 50 / 2
+  sum = 25
 
 end
 
 def multiplication(num1, num2)
+  puts 4 ** 30
+  sum = 120
 
 end
 
 def modulo(num1, num2)
+  puts 34 % 5
+  sum = 4
 
 end
 
-def square_root(num)
+def square_root(num) 
+  81.class
+  sum = 9
 
 end
